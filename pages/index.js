7d6d28bb12b0a515739aee60e1777765b8d9dbf5
Jessica da Trindade <39138578+JessicaDaTrindade@users.noreply.github.com>
2026-02-12 17:18:04 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Aninha, você é uma menina incrível e a dinda te ama 🩷</h1>
+function Home() {
+  return <h1>Aninha, você é uma menina incrível e a dinda te ama 🩷</h1>;
 }
 
 export default Home;
